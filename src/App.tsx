2bc1968ecaadedfeb65c68af5df892dfc125ffc1
Savcinschi Egor <sasha_lagun1995@mail.ru>
2022-08-20 15:44:23 +0300

    commit -1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +22,6 @@ function App() {
       </header>
     </div>
   );
-}
+} 
 
 export default App;
